c044b20d8376d7db19672829d52bc282f021a860
RosaMGini <rosa.gini@ars.toscana.it>
2022-09-02 11:19:26 +0200

    clean example 1

--- examples/example_01/test_simple.R
+++ examples/example_01/test_simple.R
@@ -1,3 +1,6 @@
+#-------------------------------
+# example 1:
+
 rm(list=ls(all.names=TRUE))
 
 #set the directory where the file is saved as the working directory
@@ -17,12 +20,13 @@ library(data.table)
 longitudinal_dataset <- fread(paste0(thisdir,"/input/longitudinal_dataset.csv"), sep = ",")
 cohort <- fread(paste0(thisdir,"/input/cohort.csv"), sep = ",")
 
+#set dates
 longitudinal_dataset[,date := as.character(date)]
 longitudinal_dataset[,date := as.Date(date,"%Y%m%d")]
 cohort[,study_entry_date := as.character(study_entry_date)]
 cohort[,study_entry_date := as.Date(study_entry_date,"%Y%m%d")]
 
-
+#generate output
 output <- MergeFilterAndCollapse(listdatasetL =  list(longitudinal_dataset),
                                  datasetS = cohort,
                                  condition= "date >= study_entry_date - 365 & date < study_entry_date",
@@ -35,4 +39,5 @@ output <- MergeFilterAndCollapse(listdatasetL =  list(longitudinal_dataset),
                                  )
 )
 
+#save output
 fwrite(output,file=paste0(thisdir,"/output/output.csv"))
